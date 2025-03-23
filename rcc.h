26a04502dac1839d9230697c01b879e7d78969f2
Stef van Stipdonk <stefvanstipdonk@gmail.com>
2025-03-23 09:31:00 +0100

    Add in reserved spaces"
    I read the datasheet wrong and didn't add any of the reserved space which it specified.
    Now the reserved space is added to the rcc_t struct using basic padding.

--- rcc.h
+++ rcc.h
@@ -13,19 +13,51 @@ struct rcc_t {
 	volatile uint32_t CIR;		// Clock Interrupt Register
 	volatile uint32_t AHB1RSTR;	// AHB1 Peripheral Reset Register
 	volatile uint32_t AHB2RSTR;	// AHB2 Peripheral Reset Register
+
+	uint32_t : 32;			// RESERVED SPACE
+	uint32_t : 32;			// RESERVED SPACE
+
 	volatile uint32_t APB1RSTR;	// APB1 Peripheral Reset Register
 	volatile uint32_t APB2RSTR;	// APB2 Peripheral Reset Register
+
+	uint32_t : 32;			// RESERVED SPACE
+	uint32_t : 32;			// RESERVED SPACE
+
 	volatile uint32_t AHB1ENR;	// AHB1 Peripheral Clock Enable Register
 	volatile uint32_t AHB2ENR;	// AHB2 Peripheral Clock Enable Register
+
+	uint32_t : 32;			// RESERVED SPACE
+	uint32_t : 32;			// RESERVED SPACE
+
+	volatile uint32_t APB1ENR;	// APB1 Peripheral Clock Enable Register
 	volatile uint32_t APB2ENR;	// APB2 Peripheral Clock Enable Register
+
+	uint32_t : 32;			// RESERVED SPACE
+	uint32_t : 32;			// RESERVED SPACE
+
 	volatile uint32_t AHB1LPENR;	// AHB1 Peripheral Clock Enable in Low Power Mode Register
 	volatile uint32_t AHB2LPENR;	// AHB2 Peripheral Clock Enable in Low Power Mode Register
+
+	uint32_t : 32;			// RESERVED SPACE
+	uint32_t : 32;			// RESERVED SPACE
+
 	volatile uint32_t APB1LPENR;	// APB1 Peripheral Clock Enable in Low Power Mode Register
 	volatile uint32_t APB2LPENR;	// APB2 Peripheral Clock Enabled in Low Power Mode Register
+
+	uint32_t : 32;			// RESERVED SPACE
+	uint32_t : 32;			// RESERVED SPACE
+
 	volatile uint32_t BDCR;		// Backup Domain Control Register
 	volatile uint32_t CSR;		// Clock Control & Status Register
+
+	uint32_t : 32;			// RESERVED SPACE
+	uint32_t : 32;			// RESERVED SPACE
+
 	volatile uint32_t SSCGR;	// Spread Spectrum Clock Generation Register
 	volatile uint32_t PLLI2SCFGR;	// PLLI2S Configuration Register
+
+	uint32_t : 32;			// RESERVED SPACE
+
 	volatile uint32_t DCKCFGR;	// Dedicated Clocks Configuration Register
 };
 
