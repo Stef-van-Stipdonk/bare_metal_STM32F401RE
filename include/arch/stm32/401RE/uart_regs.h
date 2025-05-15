4ea724850218c79c370ba0015f84a6684d9ad6bf
Stef van Stipdonk <stefvanstipdonk@gmail.com>
2025-05-15 22:22:03 +0200

    Feature 8: Add UART_SR macros

--- include/arch/stm32/401RE/uart_regs.h
+++ include/arch/stm32/401RE/uart_regs.h
@@ -3,17 +3,6 @@
 
 #include <stdint.h>
 
-#define UART_SR_PE_POS     0
-#define UART_SR_PE_WIDTH   1
-#define UART_SR_PE_MASK    (0x1 << UART_CFGR_PE_POS)
-
-#define UART_SR_SET_PE(reg, val) \
-    do { \
-        (reg) = ((reg) & ~UART_SR_PE_MASK) | (((val) & 0x1) << UART_SR_PE_POS); \
-    } while (0)
-
-#define UART_SR_GET_PE(reg) \
-    (((reg) & UART_SR_PE_MASK) >> UART_SR_PE_POS)
 
 struct uart_t {
 	volatile uint32_t SR;	// Status Register
@@ -27,5 +16,167 @@ struct uart_t {
 
 #define UART1 ((struct uart_t *) 0x40011000)
 #define UART2 ((struct uart_t *) 0x40004400)
+
+/**
+* MACROS UART_SR
+*/
+
+//
+// USART_SR_PE
+//
+#define UART_SR_PE_POS     0
+#define UART_SR_PE_WIDTH   1
+#define UART_SR_PE_MASK    (0x1 << UART_SR_PE_POS)
+
+#define UART_SR_SET_PE(reg, val) \
+    do { \
+        (reg) = ((reg) & ~UART_SR_PE_MASK) | (((val) & 0x1) << UART_SR_PE_POS); \
+    } while (0)
+
+#define UART_SR_GET_PE(reg) \
+    (((reg) & UART_SR_PE_MASK) >> UART_SR_PE_POS)
+
+//
+// USART_SR_FE
+//
+#define UART_SR_FE_POS     1
+#define UART_SR_FE_WIDTH   1
+#define UART_SR_FE_MASK    (0x1 << UART_SR_FE_POS)
+
+#define UART_SR_SET_SE(reg, val) \
+    do { \
+        (reg) = ((reg) & ~UART_SR_FE_MASK) | (((val) & 0x1) << UART_SR_FE_POS); \
+    } while (0)
+
+#define UART_SR_GET_FE(reg) \
+    (((reg) & UART_SR_FE_MASK) >> UART_SR_FE_POS)
+
+
+//
+// USART_SR_NF
+//
+#define UART_SR_NF_POS     1
+#define UART_SR_NF_WIDTH   1
+#define UART_SR_NF_MASK    (0x1 << UART_SR_NF_POS)
+
+#define UART_SR_SET_NF(reg, val) \
+    do { \
+        (reg) = ((reg) & ~UART_SR_NF_MASK) | (((val) & 0x1) << UART_SR_NF_POS); \
+    } while (0)
+
+#define UART_SR_GET_NF(reg) \
+    (((reg) & UART_SR_NF_MASK) >> UART_SR_NF_POS)
+
+
+//
+// USART_SR_ORE
+//
+#define UART_SR_ORE_POS     1
+#define UART_SR_ORE_WIDTH   1
+#define UART_SR_ORE_MASK    (0x1 << UART_SR_ORE_POS)
+
+#define UART_SR_SET_ORE(reg, val) \
+    do { \
+        (reg) = ((reg) & ~UART_SR_ORE_MASK) | (((val) & 0x1) << UART_SR_ORE_POS); \
+    } while (0)
+
+#define UART_SR_GET_ORE(reg) \
+    (((reg) & UART_SR_ORE_MASK) >> UART_SR_ORE_POS)
+
+
+//
+// USART_SR_IDLE
+//
+#define UART_SR_IDLE_POS     1
+#define UART_SR_IDLE_WIDTH   1
+#define UART_SR_IDLE_MASK    (0x1 << UART_SR_IDLE_POS)
+
+#define UART_SR_SET_IDLE(reg, val) \
+    do { \
+        (reg) = ((reg) & ~UART_SR_IDLE_MASK) | (((val) & 0x1) << UART_SR_IDLE_POS); \
+    } while (0)
+
+#define UART_SR_GET_IDLE(reg) \
+    (((reg) & UART_SR_IDLE_MASK) >> UART_SR_IDLE_POS)
+
+
+//
+// USART_SR_RXNE
+//
+#define UART_SR_RXNE_POS     1
+#define UART_SR_RXNE_WIDTH   1
+#define UART_SR_RXNE_MASK    (0x1 << UART_SR_RXNE_POS)
+
+#define UART_SR_SET_RXNE(reg, val) \
+    do { \
+        (reg) = ((reg) & ~UART_SR_RXNE_MASK) | (((val) & 0x1) << UART_SR_RXNE_POS); \
+    } while (0)
+
+#define UART_SR_GET_RXNE(reg) \
+    (((reg) & UART_SR_RXNE_MASK) >> UART_SR_RXNE_POS)
+
+
+//
+// USART_SR_TC
+//
+#define UART_SR_TC_POS     1
+#define UART_SR_TC_WIDTH   1
+#define UART_SR_TC_MASK    (0x1 << UART_SR_TC_POS)
+
+#define UART_SR_SET_TC(reg, val) \
+    do { \
+        (reg) = ((reg) & ~UART_SR_TC_MASK) | (((val) & 0x1) << UART_SR_TC_POS); \
+    } while (0)
+
+#define UART_SR_GET_TC(reg) \
+    (((reg) & UART_SR_TC_MASK) >> UART_SR_TC_POS)
+
+
+//
+// USART_SR_TXE
+//
+#define UART_SR_TXE_POS     1
+#define UART_SR_TXE_WIDTH   1
+#define UART_SR_TXE_MASK    (0x1 << UART_SR_TXE_POS)
+
+#define UART_SR_SET_TXE(reg, val) \
+    do { \
+        (reg) = ((reg) & ~UART_SR_TXE_MASK) | (((val) & 0x1) << UART_SR_TXE_POS); \
+    } while (0)
+
+#define UART_SR_GET_TXE(reg) \
+    (((reg) & UART_SR_TXE_MASK) >> UART_SR_TXE_POS)
+
+
+//
+// USART_SR_LBD
+//
+#define UART_SR_LBD_POS     1
+#define UART_SR_LBD_WIDTH   1
+#define UART_SR_LBD_MASK    (0x1 << UART_SR_LBD_POS)
+
+#define UART_SR_SET_LBD(reg, val) \
+    do { \
+        (reg) = ((reg) & ~UART_SR_LBD_MASK) | (((val) & 0x1) << UART_SR_LBD_POS); \
+    } while (0)
+
+#define UART_SR_GET_LBD(reg) \
+    (((reg) & UART_SR_LBD_MASK) >> UART_SR_LBD_POS)
+
+
+//
+// USART_SR_CTS
+//
+#define UART_SR_CTS_POS     1
+#define UART_SR_CTS_WIDTH   1
+#define UART_SR_CTS_MASK    (0x1 << UART_SR_CTS_POS)
+
+#define UART_SR_SET_CTS(reg, val) \
+    do { \
+        (reg) = ((reg) & ~UART_SR_CTS_MASK) | (((val) & 0x1) << UART_SR_CTS_POS); \
+    } while (0)
+
+#define UART_SR_GET_CTS(reg) \
+    (((reg) & UART_SR_CTS_MASK) >> UART_SR_CTS_POS)
 
 #endif // UART_REGS_H
