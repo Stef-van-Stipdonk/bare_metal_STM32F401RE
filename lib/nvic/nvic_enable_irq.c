dd2f41ade9a1070097647397b91578849e39bd03
Stef van Stipdonk <stefvanstipdonk@gmail.com>
2025-06-01 19:50:25 +0200

    Feature 16: Setup basic nvic, enable/disable irq

---
+++ lib/nvic/nvic_enable_irq.c
@@ -0,0 +1,46 @@
+#include "nvic.h"
+#include "nvic_regs.h"
+
+#define ISER0_BOUNDARY 31
+#define ISER1_BOUNDARY (ISER0_BOUNDARY + 32)
+#define ISER2_BOUNDARY (ISER1_BOUNDARY + 32)
+#define ISER3_BOUNDARY (ISER2_BOUNDARY + 32)
+#define ISER4_BOUNDARY (ISER3_BOUNDARY + 32)
+#define ISER5_BOUNDARY (ISER4_BOUNDARY + 32)
+#define ISER6_BOUNDARY (ISER5_BOUNDARY + 32)
+#define ISER7_BOUNDARY (ISER6_BOUNDARY + 32)
+
+void nvic_enable_irq(uint32_t IRQ_number) {
+	if (IRQ_number > ISER7_BOUNDARY) {
+		return;
+	}
+
+	if (IRQ_number < ISER0_BOUNDARY) {
+		NVIC->ISER0 = (1 << IRQ_number);
+	}
+	else if (IRQ_number < ISER1_BOUNDARY) {
+		NVIC->ISER1 = (1 << IRQ_number);
+	}
+	else if (IRQ_number < ISER2_BOUNDARY) {
+		NVIC->ISER2 = (1 << IRQ_number);
+	}
+	else if (IRQ_number < ISER3_BOUNDARY) {
+		NVIC->ISER3 = (1 << IRQ_number);
+	}
+	else if (IRQ_number < ISER4_BOUNDARY) {
+		NVIC->ISER4 = (1 << IRQ_number);
+	}
+	else if (IRQ_number < ISER5_BOUNDARY) {
+		NVIC->ISER5 = (1 << IRQ_number);
+	}
+	else if (IRQ_number < ISER6_BOUNDARY) {
+		NVIC->ISER6 = (1 << IRQ_number);
+	}
+	else if (IRQ_number < ISER7_BOUNDARY) {
+		if (IRQ_number > ISER7_BOUNDARY + 15) { // bits 16 to 31 are reserved
+			return;
+		}
+
+		NVIC->ISER7 = (1 << IRQ_number);
+	}
+}
