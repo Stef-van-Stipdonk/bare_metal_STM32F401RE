467a6686989aa958e0966a107f3d6b6b23dc240e
Stef van Stipdonk <stefvanstipdonk@gmail.com>
2025-07-24 19:30:57 +0200

    Feature #36: Add asm for getting registers

--- main.c
+++ main.c
@@ -9,10 +9,60 @@
 
 extern volatile struct circular_buffer uart_receive_buffer;
 
+static uint32_t r[16];
+static uint32_t cpsr;
+
+void IRQ_HardFaultHandler() { 
+    asm volatile (
+        "mov %[r0], r0\n"
+        "mov %[r1], r1\n"
+        "mov %[r2], r2\n"
+        "mov %[r3], r3\n"
+        "mov %[r4], r4\n"
+        "mov %[r5], r5\n"
+        "mov %[r6], r6\n"
+        "mov %[r7], r7\n"
+        "mov %[r8], r8\n"
+        "mov %[r9], r9\n"
+        "mov %[r10], r10\n"
+        "mov %[r11], r11\n"
+        "mov %[r12], r12\n"
+        "mov %[sp], sp\n"
+        "mov %[lr], lr\n"
+        "mov %[pc], pc\n"
+        "mrs %[cpsr], cpsr\n"
+        : [r0]  "=r" (r[0]),
+          [r1]  "=r" (r[1]),
+          [r2]  "=r" (r[2]),
+          [r3]  "=r" (r[3]),
+          [r4]  "=r" (r[4]),
+          [r5]  "=r" (r[5]),
+          [r6]  "=r" (r[6]),
+          [r7]  "=r" (r[7]),
+          [r8]  "=r" (r[8]),
+          [r9]  "=r" (r[9]),
+          [r10] "=r" (r[10]),
+          [r11] "=r" (r[11]),
+          [r12] "=r" (r[12]),
+          [sp]  "=r" (r[13]),
+          [lr]  "=r" (r[14]),
+          [pc]  "=r" (r[15]),
+          [cpsr] "=r" (cpsr)
+    );
+}
+
+
 int main(void) {
 	rcc_init();
 	uart_init(UART2, 115200);
 	uart_write_buffer(UART2, "[INFO] UART initialized\r\n");
+
+
+
+    for (int i = 0; i < 16; i++) {
+        uart_write_byte(UART2, (uint8_t)r[i]);
+    }
+
 
     uint16_t tmp = uart_receive_buffer.head; 
 
